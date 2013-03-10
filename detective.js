572795b7603950b6afa75bfdb6286a4be0b6d878
John Leonard <john.leonard@frogdesign.com>
2013-03-10 19:22:10 -0400

    adding print() function

--- detective.js
+++ detective.js
@@ -1,6 +1,6 @@
 /**
 *
-* Detective v0.1 – Simple browser and os detection for javascript.
+* Detective v0.1 – Simple userAgent browser and os detection for javascript.
 * github.com/jleonard/detective
 * 
 * Permission is hereby granted, free of charge, to any person
@@ -29,23 +29,61 @@
 *
 */
 
+(function(){
+
+  var OSX = {
+    "10_1" : "Puma",
+    "10_2" : "Jaguar",
+    "10_3" : "Panther",
+    "10_4" : "Tiger",
+    "10_5" : "Leopard",
+    "10_6" : "Snow Leopard",
+    "10_7" : "Lion",
+    "10_8" : "Mountain Lion"
+  }
+
   window.Detective = {
     browser :{
-      name : null,
-      version : null,
-      engine : null
+      name : "",
+      version : "",
+      engine : ""
     },
     os : {
-      name : null,
-      version : null
+      name : "",
+      version : ""
     },
     device : {
-      name : null
+      name : ""
+    },
+    print: function(){
+      var el = document.createElement("div");
+      el.setAttribute("id","Detective");
+
+      var os = document.createElement("p");
+      os.appendChild(document.createTextNode(this.os.name.toUpperCase()+" "+this.os.version));
+      os.style.margin = "0px";
+
+      var browser = document.createElement("p");
+      browser.appendChild(document.createTextNode(this.browser.name+" v"+this.browser.version));
+      browser.style.margin = "0px";
+
+      el.appendChild(os);
+      el.appendChild(browser);
+
+      if(this.device.name){
+        var device = document.createElement("p");
+        device.appendChild(document.createTextNode(this.device.name));
+        el.appendChild(device);
+      }
+      el.style.padding = "10px";
+      el.style.position = "absolute";
+      el.style.top = "10px";
+      el.style.right = "10px";
+      el.style.color = "#fff";
+      el.style.fontFamily = "sans-serif";
+      document.body.appendChild(el);
     }
   };
-
-window.onload = function(){
-
   var regex = navigator.userAgent.match(/Safari/);
   if(regex){
     Detective.browser.engine = "webkit";
@@ -63,7 +101,7 @@ window.onload = function(){
   if(regex){
     Detective.browser.engine = "presto";
     Detective.browser.name = "opera";
-    var version = navigator.userAgent.match(/Version\/\d{1,3}/);
+    var version = navigator.userAgent.match(/Version\/\d{1,3}.\d{1,3}/);
     if(version){
       version = version[0].split("/");
       Detective.browser.version = version[1];
@@ -143,12 +181,21 @@ window.onload = function(){
     Detective.isIOS = true;
   }
 
-  var regex = navigator.userAgent.match(/Intel Mac OS X/);
+  var split = "_";
+  if(Detective.browser.name == "firefox" || Detective.browser.name == "opera"){
+    regex = navigator.userAgent.match(/Intel Mac OS X \d{1,3}.\d{1,3}/);
+    split = ".";
+  }else{
+    regex = navigator.userAgent.match(/Intel Mac OS X \d{1,3}_\d{1,3}_\d{1,3}/);
+  }
   if(regex){
+    var version = regex[0].split("Intel Mac OS X ");
+    version_array = version[1].split(split);
+    Detective.os.version = OSX[version_array[0] + "_" + version_array[1]] + " "+version[1];
+    Detective.os.version.replace(/_/g,".");
     Detective.os.name = "osx";
     Detective.isOSX = true;
   }
-
 
   var regex = navigator.userAgent.match(/Android \d{1,3}.\d{1,3}/);
   if(regex){
@@ -162,7 +209,7 @@ window.onload = function(){
   if(Detective.os.name){
     _classes += Detective.os.name;
     if(Detective.os.version){
-      _classes += " " + Detective.os.name + "-" + Detective.os.version.replace(".","-");
+      _classes += " " + Detective.os.name + "-" + Detective.os.version.replace(".","-").toLowerCase();
     }
   }
 
@@ -183,4 +230,5 @@ window.onload = function(){
 
   var el = document.getElementsByTagName("html");
   el[0].className = el[0].className + _classes;
-}
+
+})();
